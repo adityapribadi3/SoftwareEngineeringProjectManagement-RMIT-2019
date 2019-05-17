dee85643c5f70360bd0f01c81ed42f3235ce0c04
adityapribadi3 <r.aditya.pribadi.k@gmail.com>
2019-05-17 17:34:16 +1000

    Update flask_api.py

--- seapi/flask_api.py
+++ seapi/flask_api.py
@@ -2,8 +2,10 @@ from flask import Flask, Blueprint, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os, requests, json
+from flask_cors import CORS
 
 api = Blueprint("api", __name__)
+CORS(api)
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -70,7 +72,43 @@ def getDrinks():
     result = drinkSchema.dump(drinks)
     return jsonify(result.data)
 
+#REGISTER
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.Text)
+    password = db.Column(db.Text)
+    email = db.Column(db.Text)
 
+    def __init__(self, name, password, email, id = None):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.email = email
+
+class UserSchema(ma.Schema):
+    
+    def __init__(self, strict = True, **kwargs):
+        super().__init__(strict = strict, **kwargs)
+    
+    class Meta:
+        fields = ("id","name", "password", "email")
+
+userSchema = UserSchema()
+
+# Endpoint to register user.
+@api.route("/register", methods = ["POST"])
+def register():
+    name = request.json["name"]
+    password = request.json["password"]
+    email = request.json["email"]
+
+    newUser = User(name, password, email)
+
+    db.session.add(newUser)
+    db.session.commit()
+
+    return userSchema.jsonify(newUser)
 
 
 
